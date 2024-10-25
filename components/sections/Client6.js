d1b4b5bff0ec2dab7a4ab72f505b3b65ccede1d2
MrLokesh161 <lokesh07084@gmail.com>
2024-10-25 10:56:32 +0530

    Initial Commit

---
+++ components/sections/Client6.js
@@ -0,0 +1,85 @@
+import Link from "next/link"
+
+export default function Client6() {
+    return (
+        <>
+            <div className="client10-section-area section-padding8">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 m-auto">
+                            <div className="client10-header text-center margin-b60">
+                                <span className="font-16 lineh-16 weight-600 color-h10 about10span d-inline-block margin-b20">How It Works</span>
+                                <h1 className="font-44 lineh-54 weight-600 colorhp3 font-lora margin-b24s">Our Tax & Business Advisory Process</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="worksall-section-area">
+                        <img src="/assets/images/elementor/elementor67.png" alt="" className="elementors67" />
+                        <div className="row">
+                            <div className="col-lg-3" data-aos="zoom-in" data-aos-duration={1000} data-aos-easing="linear">
+                                <div className="client10-boxarea text-center">
+                                    <div className="client10-icon margin-b20 ts">
+                                        <img src="/assets/images/elementor/elementor64.png" alt="" className="elemnetors64 keyframe3" />
+                                        <div className="service10-icon ts">
+                                            <img src="/assets/images/icons/group11.svg" alt="" className="ts" />
+                                        </div>
+                                        <div className="numbericon ts">
+                                            <p className="font-16 lineh-24 font-ks weight-700 color r ts">1</p>
+                                        </div>
+                                    </div>
+                                    <Link href="/servicemiddle" className="font-lora font-20 lineh-20 weight-600 colorhp3 ts d-inline-block margin-b">Initial Consultation</Link>
+                                    <p className="font-ks font-16 lineh-26 weight-500 color-p10 ts">We evaluate your current financial situation and business needs.</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-3" data-aos="zoom-out" data-aos-duration={1200} data-aos-easing="linear">
+                                <div className="client10-boxarea text-center positionboxarea">
+                                    <div className="client10-icon margin-b20 ts">
+                                        <img src="/assets/images/elementor/elementor64.png" alt="" className="elemnetors64 keyframe3" />
+                                        <div className="service10-icon ts">
+                                            <img src="/assets/images/icons/group10.svg" alt="" className="ts" />
+                                        </div>
+                                        <div className="numbericon ts">
+                                            <p className="font-16 lineh-24 font-ks weight-700 color r ts">2</p>
+                                        </div>
+                                    </div>
+                                    <Link href="/servicemiddle" className="font-lora font-20 lineh-20 weight-600 colorhp3 ts d-inline-block margin-b">Customized Tax Strategy</Link>
+                                    <p className="font-ks font-16 lineh-26 weight-500 color-p10 ts">We develop a tailored tax and business advisory plan based on your goals.</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-3" data-aos="zoom-in" data-aos-duration={1400} data-aos-easing="linear">
+                                <div className="client10-boxarea text-center">
+                                    <div className="client10-icon margin-b20 ts">
+                                        <img src="/assets/images/elementor/elementor64.png" alt="" className="elemnetors64 keyframe3" />
+                                        <div className="service10-icon ts">
+                                            <img src="/assets/images/icons/group9.svg" alt="" className="ts" />
+                                        </div>
+                                        <div className="numbericon ts">
+                                            <p className="font-16 lineh-24 font-ks weight-700 color r ts">3</p>
+                                        </div>
+                                    </div>
+                                    <Link href="/servicemiddle" className="font-lora font-20 lineh-20 weight-600 colorhp3 ts d-inline-block margin-b">Implementation</Link>
+                                    <p className="font-ks font-16 lineh-26 weight-500 color-p10 ts">We execute the plan, ensuring compliance and optimizing tax savings.</p>
+                                </div>
+                            </div>
+                            <div className="col-lg-3" data-aos="zoom-in" data-aos-duration={1600} data-aos-easing="linear">
+                                <div className="client10-boxarea text-center positionboxarea">
+                                    <div className="client10-icon margin-b20 ts">
+                                        <img src="/assets/images/elementor/elementor64.png" alt="" className="elemnetors64 keyframe3" />
+                                        <div className="service10-icon ts">
+                                            <img src="/assets/images/icons/group8.svg" alt="" className="ts" />
+                                        </div>
+                                        <div className="numbericon ts">
+                                            <p className="font-16 lineh-24 font-ks weight-700 color r ts">4</p>
+                                        </div>
+                                    </div>
+                                    <Link href="/servicemiddle" className="font-lora font-20 lineh-20 weight-600 colorhp3 ts d-inline-block margin-b">Ongoing Support</Link>
+                                    <p className="font-ks font-16 lineh-26 weight-500 color-p10 ts">We provide continuous support to adapt your plan as your business evolves.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ components/sections/Service13.js
@@ -0,0 +1,108 @@
+import Link from "next/link"
+
+export default function Service13() {
+    return (
+        <>
+            <div className="service10-section-area section-padding8">
+                <img src="/assets/images/elementor/elementor62.png" alt="" className="elementors62" />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 m-auto">
+                            <div className="service10-header text-center margin-b60">
+                                <span className="font-16 lineh-16 weight-600 color-h10 about10span d-inline-block margin-b20">Our Services</span>
+                                <h1 className="font-44 lineh-54 weight-600 colorhp3 font-lora margin-b24s">Tailored Tax & Business Solutions</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration={800} data-aos-easing="linear">
+                            <div className="service10-boxarea text-center">
+                                <div className="welcome-ellipsbox ts">
+                                    <img src="/assets/images/elementor/elementor64.png" alt="" className="keyframe3 ts" />
+                                    <div className="elips-icon6 ts">
+                                        <img src="/assets/images/icons/group7.svg" alt="" className="ts" />
+                                    </div>
+                                </div>
+                                <Link href="/tax-planning" className="font-lora font-24 weight-600 colorhp3 d-inline-block margin-b ts">Tax Planning</Link>
+                                <p className="font-ks font-16 weight-500 color-p10 lineh-26 margin-b24 ts ">Strategic tax planning to minimize your tax liability and optimize financial outcomes.</p>
+                                <Link href="/tax-planning" className="font-16 font-ks weight-700 colorhp3 lineh-16 ts">Learn More
+                                    <span><img src="/assets/images/icons/arrowben2.svg" alt="" className="ts" /></span></Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration={1000} data-aos-easing="linear">
+                            <div className="service10-boxarea text-center">
+                                <div className="welcome-ellipsbox ts">
+                                    <img src="/assets/images/elementor/elementor64.png" alt="" className="keyframe3 ts" />
+                                    <div className="elips-icon6 ts">
+                                        <img src="/assets/images/icons/group8.svg" alt="" className="ts" />
+                                    </div>
+                                </div>
+                                <Link href="/tax-preparation" className="font-lora font-24 weight-600 colorhp3 d-inline-block margin-b ts">Tax Preparation</Link>
+                                <p className="font-ks font-16 weight-500 color-p10 lineh-26 margin-b24 ts">Comprehensive individual and business tax return filing with a focus on accuracy and compliance.</p>
+                                <Link href="/tax-preparation" className="font-16 font-ks weight-700 colorhp3 lineh-16 ts">Learn More
+                                    <span><img src="/assets/images/icons/arrowben2.svg" alt="" className="ts" /></span></Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration={1200} data-aos-easing="linear">
+                            <div className="service10-boxarea text-center">
+                                <div className="welcome-ellipsbox ts">
+                                    <img src="/assets/images/elementor/elementor64.png" alt="" className="keyframe3 ts" />
+                                    <div className="elips-icon6 ts">
+                                        <img src="/assets/images/icons/group9.svg" alt="" className="ts" />
+                                    </div>
+                                </div>
+                                <Link href="/itin-assistance" className="font-lora font-24 weight-600 colorhp3 d-inline-block margin-b ts">ITIN Assistance</Link>
+                                <p className="font-ks font-16 weight-500 color-p10 lineh-26 margin-b24 ts">Get help with Individual Taxpayer Identification Number (ITIN) applications and renewals.</p>
+                                <Link href="/itin-assistance" className="font-16 font-ks weight-700 colorhp3 lineh-16 ts">Learn More
+                                    <span><img src="/assets/images/icons/arrowben2.svg" alt="" className="ts" /></span></Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration={1400} data-aos-easing="linear">
+                            <div className="service10-boxarea text-center">
+                                <div className="welcome-ellipsbox ts">
+                                    <img src="/assets/images/elementor/elementor64.png" alt="" className="keyframe3 ts" />
+                                    <div className="elips-icon6 ts">
+                                        <img src="/assets/images/icons/group10.svg" alt="" className="ts" />
+                                    </div>
+                                </div>
+                                <Link href="/audit-assistance" className="font-lora font-24 weight-600 colorhp3 d-inline-block margin-b ts">Audit Assistance</Link>
+                                <p className="font-ks font-16 weight-500 color-p10 lineh-26 margin-b24 ts">Representation and guidance in tax audits to help you navigate the process with confidence.</p>
+                                <Link href="/audit-assistance" className="font-16 font-ks weight-700 colorhp3 lineh-16 ts">Learn More
+                                    <span><img src="/assets/images/icons/arrowben2.svg" alt="" className="ts" /></span></Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration={1600} data-aos-easing="linear">
+                            <div className="service10-boxarea text-center">
+                                <div className="welcome-ellipsbox ts">
+                                    <img src="/assets/images/elementor/elementor64.png" alt="" className="keyframe3 ts" />
+                                    <div className="elips-icon6 ts">
+                                        <img src="/assets/images/icons/group11.svg" alt="" className="ts" />
+                                    </div>
+                                </div>
+                                <Link href="/bookkeeping" className="font-lora font-24 weight-600 colorhp3 d-inline-block margin-b ts">Bookkeeping</Link>
+                                <p className="font-ks font-16 weight-500 color-p10 lineh-26 margin-b24 ts">Accurate bookkeeping services to ensure your financials are well-organized and compliant.</p>
+                                <Link href="/bookkeeping" className="font-16 font-ks weight-700 colorhp3 lineh-16 ts">Learn More
+                                    <span><img src="/assets/images/icons/arrowben2.svg" alt="" className="ts" /></span></Link>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-duration={1800} data-aos-easing="linear">
+                            <div className="service10-boxarea text-center">
+                                <div className="welcome-ellipsbox ts">
+                                    <img src="/assets/images/elementor/elementor64.png" alt="" className="keyframe3 ts" />
+                                    <div className="elips-icon6 ts">
+                                        <img src="/assets/images/icons/group12.svg" alt="" className="ts" />
+                                    </div>
+                                </div>
+                                <Link href="/payroll" className="font-lora font-24 weight-600 colorhp3 d-inline-block margin-b ts">Payroll Services</Link>
+                                <p className="font-ks font-16 weight-500 color-p10 lineh-26 margin-b24 ts">Full-service payroll management for employees and contractors with timely tax filing and reporting.</p>
+                                <Link href="/payroll" className="font-16 font-ks weight-700 colorhp3 lineh-16 ts">Learn More
+                                    <span className="ts"><img src="/assets/images/icons/arrowben2.svg" alt="" className="ts" /></span></Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <img src="/assets/images/elementor/elementor63.png" alt="" className="elementors63" />
+            </div>
+        </>
+    )
+}
